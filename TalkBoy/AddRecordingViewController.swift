da94a9e0c295f4e2ea79e17cd3e3e43c48547b9b
Unknown <patriciaharris@hotmail.com>
2017-08-20 15:02:14 -0700

    Finished.
    Audio adding, playing, recording, and removal complete.

--- TalkBoy/AddRecordingViewController.swift
+++ TalkBoy/AddRecordingViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class AddRecordingViewController: UIViewController {
 
@@ -15,17 +16,125 @@ class AddRecordingViewController: UIViewController {
     @IBOutlet weak var addSoundButton: UIButton!
     @IBOutlet weak var titleTextField: UITextField!
     
+    var audioRecorder : AVAudioRecorder?
+    var audioPlayer : AVAudioPlayer?
+    
+    var audioURL : URL?
+    
+    // Private function instaead of copying and pasting this.
+    func setControlStateWhileRecording(isEnabled: Bool) {
+        playSoundButton.isEnabled = isEnabled
+        addSoundButton.isEnabled = isEnabled
+        titleTextField.isEnabled = isEnabled
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 1. Create the Audio Session
+        let session = AVAudioSession.sharedInstance()
+        try? session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        // Override the speaker so that it doesn't come out the headset
+        try? session.overrideOutputAudioPort(.speaker)
+        
+        try? session.setActive(true)
+        
+        // 2. Setup a URL to save the audio recording
+        // First we need access to the documents directory.
+        if let basePath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+            let pathComponents = [basePath, "audo.m4a"]
+            
+            if let audioURL = NSURL.fileURL(withPathComponents: pathComponents) {
+                
+                // Added this to store the URL needed to play the audio.  See below.
+                self.audioURL = audioURL
+                
+                // 3. Create audo settings - set initially to an empty dictionary.
+                var settings : [String : Any] = [:]
+                settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC)
+                
+                // This sets the quality of the recording
+                settings[AVSampleRateKey] = 44100.0
+                
+                settings[AVNumberOfChannelsKey] = 2
+
+                // 4. Create the Audio Recorder (with a pause here to learn about dictionaries).
+                // The constructor takes a [String : Any] dictionary
+                audioRecorder = try? AVAudioRecorder(url: audioURL, settings: settings)
+                audioRecorder?.prepareToRecord()
+            }
+        }
+        
+        // Initially, the other buttons are disabled until a sound is recorded.
+        setControlStateWhileRecording(isEnabled: false)
     }
 
     @IBAction func recordSoundButtonDidTap(_ sender: Any) {
+        
+        if let audioRecorder = audioRecorder {
+            if audioRecorder.isRecording {
+                
+                audioRecorder.stop()
+                
+                recordSoundButton.setTitle("Record Sound", for: .normal)
+                setControlStateWhileRecording(isEnabled: true)
+
+            }
+            else {
+                recordSoundButton.setTitle("Stop Recording", for: .normal)
+                setControlStateWhileRecording(isEnabled: false)
+                
+                audioRecorder.record()
+            }
+        }
     }
     
     @IBAction func playSoundButtonDidTap(_ sender: Any) {
+        
+        if let audioURL = self.audioURL {
+            audioPlayer = try? AVAudioPlayer(contentsOf: audioURL)
+            audioPlayer?.play()
+        }
     }
     
     @IBAction func addRecordingDidTap(_ sender: Any) {
+        // Get the managed object context
+        if let context  =
+            ((UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext) {
+            
+            let item = SoundEntity(entity: SoundEntity.entity(), insertInto: context)
+            item.title = titleTextField.text
+            
+            // TODO - audio data
+            if let audioURL = self.audioURL {
+                item.audioData = try? Data(contentsOf: audioURL)
+
+                // This can also be handled this way:
+                // try? context.save
+                // But then there's no handling the failure case...
+                // To get the data to "reload" we need to handle
+                // viewWillAppear.
+                do {
+                    try context.save()
+                }
+                catch {
+                    self.showAlert(message: "Data could not be saved!  Please try again.")
+                }
+                
+                navigationController?.popViewController(animated: true)
+            }
+        }
     }
     
+    // Utility function (not part of tutorial)
+    func showAlert(message: String) {
+        let alertController = UIAlertController(title: "Oops!", message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
+    }
+
 }
--- TalkBoy/TalkBoyTableViewController.swift
+++ TalkBoy/TalkBoyTableViewController.swift
@@ -7,14 +7,74 @@
 //
 
 import UIKit
+import AVFoundation
 
 class TalkBoyTableViewController: UITableViewController {
+
+    var sounds : [SoundEntity] = []
+    
+    var audioPlayer : AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 40
+
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Better to call this here...that way, when we return
+        // back to this view, it is called again, not just on
+        // first launch.
+        getSounds()
+    }
+    
+    // Get the Items from Core Data
+    func getSounds() {
+        
+        if let context  =
+            ((UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext) {
+            
+            do {
+                let coreDataItems = try context.fetch(SoundEntity.fetchRequest()) as? [SoundEntity]
+                
+                if let coreDataItems = coreDataItems {
+                    // We can just set our item array to the one given
+                    // to us by Core Data
+                    self.sounds = coreDataItems
+                }
+                
+                tableView.reloadData()
+            }
+            catch {
+                showAlert(message: "Could not retrieve the TalkBoys items from the database!  Please try again.")
+                return
+            }
+        }
+    }
+
+    func removeItem(soundEntity: SoundEntity) {
+        
+        // Get the managed object context
+        if let context  =
+            ((UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext) {
+            
+            context.delete(soundEntity)
+            
+        }
+    }
+
+    // Utility function (not part of tutorial)
+    func showAlert(message: String) {
+        let alertController = UIAlertController(title: "Oops!", message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
+    }
 
     // MARK: - Table view data source
 
@@ -23,18 +83,34 @@ class TalkBoyTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return sounds.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "AddRecordingCell", for: indexPath)
 
         // Configure the cell...
+        let soundItem = sounds[indexPath.row]
+        cell.textLabel?.text = soundItem.title
 
         return cell
     }
 
+    // Allow the user to tap on a row and play the sound
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let sound = sounds[indexPath.row]
+        if let audioData = sound.audioData {
+            
+            audioPlayer = try? AVAudioPlayer(data: audioData)
+            audioPlayer?.play()
+        }
+        
+        // Deselect the row so that it's not still a selected color.
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
     // Override to support conditional editing of the table view.
     // This allows removal of the selected row.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -50,7 +126,14 @@ class TalkBoyTableViewController: UITableViewController {
                             forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            // tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            // 1. Remove the item from Core Data
+            let soundItem = sounds[indexPath.row]
+            removeItem(soundEntity: soundItem)
+            
+            // 2. Re-get all the Core Data
+            getSounds()
         }
         else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array,
